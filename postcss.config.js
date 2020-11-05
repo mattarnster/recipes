4f1614535f327e41130f1224328d5a9f8eaab6ab
Matt Arnold <mattarnster.co.uk@gmail.com>
2020-11-05 14:44:53 -0500

    Postcss weirdness?

--- postcss.config.js
+++ postcss.config.js
@@ -19,7 +19,6 @@ const purgecss = [
   module.exports = {
     plugins: [
       'tailwindcss',
-      process.env.NODE_ENV === 'production' ? purgecss : undefined,
       'postcss-preset-env',
     ],
   };
